0943dad28f2022b717fb6673b8a4bb268649d475
Ryan Turner <ryanthepit@gmail.com>
2017-05-24 18:13:23 -0700

    fixed .get method

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -5,18 +5,15 @@ var app = app || {};
   const repos = {};
 
   repos.all = [];
-  // TODO: Refactor your ajax call to use the $.get method, and make a request to our new proxy route.
-   //       Don't forget to remove the headers from our request - we're no longer using a token on the
-   //       client side of our app, our new proxyGitHub function will be handling the token using our
-   //       new environment variable!
 
   repos.requestRepos = function(callback) {
-    $.get('/github', function(data){
-      repos.all(data);
-      callback(data);
-    }, function(err){
-      console.error(err);
-    });
+    // TODO: Refactor your ajax call to use the $.get method, and make a request to our new proxy route.
+    //       Don't forget to remove the headers from our request - we're no longer using a token on the
+    //       client side of our app, our new proxyGitHub function will be handling the token using our
+    //       new environment variable!
+    $.get('/github/user/repos')
+    .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
+    .then(callback);
   };
 
   repos.with = attr => repos.all.filter(repo => repo[attr]);
